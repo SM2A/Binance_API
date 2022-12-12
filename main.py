510db9218a68666ef8afbb43ff833c85f7960c60
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-12-12 10:39:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-result = requests.get("https://api.binance.com/api/v3/exchangeInfo")
+result = requests.get("https://api.binance.com/api/v2/exchangeInfo")
 
 with open('exchangeInfo.json', 'w') as outfile:
     outfile.write(result.text)
