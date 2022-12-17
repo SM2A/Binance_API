b7b6b3c4e5ae96d46ca5f3b2279f1ee5bcd7d857
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-12-17 07:02:06 +0000

    Prettry save json result

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import requests
 
 result = requests.get("https://api.binance.com/api/v3/exchangeInfo")
 
+json_text = json.loads(result.text)
+
 with open('exchangeInfo.json', 'w') as outfile:
-    outfile.write(result.text)
+    outfile.write(json.dumps(json_text, indent=4))
