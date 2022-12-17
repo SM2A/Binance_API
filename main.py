b803764844316ba6bae4ffa6975073a89ac9fc97
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-12-17 07:23:40 +0000

    Save pretty and compact result

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ result = requests.get("https://api.binance.com/api/v3/exchangeInfo")
 json_text = json.loads(result.text)
 
 if len(result.text) > 1000 :
-    with open('exchangeInfo.json', 'w') as outfile:
+    with open('exchangeInfoPretty.json', 'w') as outfile:
         outfile.write(json.dumps(json_text, indent=4))
+    with open('exchangeInfoCompact.json', 'w') as outfile:
+        outfile.write(result.text)
